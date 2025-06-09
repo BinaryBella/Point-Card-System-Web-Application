695324af2c2bf3ecd9884ca35ae2fce4838c64a1
Chathushi Kavindya <chathushikavindya09@gmail.com>
2025-06-09 12:39:55 +0530

    modify dashboard to get total pointss and claiming points

--- Point Card System/Default.aspx.cs
+++ Point Card System/Default.aspx.cs
@@ -1,4 +1,40 @@
-﻿using System;
+﻿//using System;
+//using System.Web.UI;
+//using Point_Card_System.DAL;
+
+//namespace Point_Card_System
+//{
+//    public partial class _Default : Page
+//    {
+//        protected int RegisteredCustomerCount { get; set; }
+
+//        protected void Page_Load(object sender, EventArgs e)
+//        {
+//            if (!IsPostBack)
+//            {
+//                LoadDashboardData();
+//            }
+//        }
+
+//        private void LoadDashboardData()
+//        {
+//            try
+//            {
+//                CustomerController cuscon = new CustomerController();
+//                RegisteredCustomerCount = cuscon.GetRegisteredCustomerCount();
+//            }
+//            catch (Exception ex)
+//            {
+//                // Handle error - you might want to show a user-friendly message
+//                RegisteredCustomerCount = 0;
+//                // Log the exception or show error message
+//                // Response.Write("<script>alert('Error loading dashboard data: " + ex.Message + "');</script>");
+//            }
+//        }
+//    }
+//}
+
+using System;
 using System.Web.UI;
 using Point_Card_System.DAL;
 
@@ -7,6 +43,8 @@ namespace Point_Card_System
     public partial class _Default : Page
     {
         protected int RegisteredCustomerCount { get; set; }
+        protected double TotalPointsIssued { get; set; }
+        protected double TotalClaimablePointsIssued { get; set; }
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -21,13 +59,20 @@ namespace Point_Card_System
             try
             {
                 CustomerController cuscon = new CustomerController();
+
                 RegisteredCustomerCount = cuscon.GetRegisteredCustomerCount();
+
+                var pointsDashboard = cuscon.GetPointsDashboard();
+                TotalPointsIssued = pointsDashboard.TotalPointsIssued;
+                TotalClaimablePointsIssued = pointsDashboard.TotalClaimablePointsIssued;
             }
             catch (Exception ex)
             {
-                // Handle error - you might want to show a user-friendly message
                 RegisteredCustomerCount = 0;
-                // Log the exception or show error message
+                TotalPointsIssued = 0;
+                TotalClaimablePointsIssued = 0;
+
+                // Optionally log or show error message
                 // Response.Write("<script>alert('Error loading dashboard data: " + ex.Message + "');</script>");
             }
         }
